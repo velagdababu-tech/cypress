7ac3e8d65fb441b551c91dee99f69f502c1c54ab
velagdababu-tech <velagadababu@gmail.com>
2025-10-21 18:32:53 +0530

    added few it blocks

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -5,4 +5,11 @@ describe('Google Search', () => {
     cy.title().should('include', 'Google');
     cy.allure().step('Checked the page title');
   });
+
+    it('should have correct title', () => {
+    cy.allure().label('feature', 'Google Search');
+    cy.visit('/');
+    cy.get('[class="lnXdpd"]').should('exist');
+    cy.allure().step('Checked the locator exist');
+  });
 });
