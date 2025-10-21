54ed29e010558fc40ed50c268d149e70a4e6956d
velagdababu-tech <velagadababu@gmail.com>
2025-10-21 17:00:35 +0530

    allure reports

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,9 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // register Allure reporter plugin
       allureWriter(on, config);
-
-      // you can add other node event listeners here if needed
       return config;
     },
-    specPattern: "cypress/e2e/**/*.cy.js", // your test file pattern
+    specPattern: "cypress/e2e/**/*.cy.js",
   },
-  reporter: 'cypress-mochawesome-reporter', // optional, if you also want Mochawesome
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    charts: true,
-    overwrite: false,
-    html: true,
-    json: true
-  }
 });
