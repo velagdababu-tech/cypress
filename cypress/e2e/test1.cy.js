75dd128c90f45cd035f07843456397d1020bbffa
velagdababu-tech <velagadababu@gmail.com>
2025-10-22 11:17:36 +0530

    added readme file data and updated the test case

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,15 +1,11 @@
-describe('Google Search', () => {
-  it('should have correct title', () => {
-    cy.allure().label('feature', 'Google Search');
-    cy.visit('/');
-    cy.title().should('include', 'Google');
-    cy.allure().step('Checked the page title');
+describe("Google Search", () => {
+  it("should have correct title", () => {
+    cy.visit("/");
+    cy.title().should("include", "Google");
   });
 
-    it('should have correct title', () => {
-    cy.allure().label('feature', 'Google Search');
-    cy.visit('/');
-    cy.get('[class="lnXdpd"]').should('exist');
-    cy.allure().step('Checked the locator exist');
+  it("should have correct title", () => {
+    cy.visit("/");
+    cy.get('[class="lnXdpd"]').should("exist");
   });
 });
